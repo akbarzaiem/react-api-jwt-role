8f9aae37853606964d94467db0b6cafbb1bcff09
Buanasb <buanacopter@gmail.com>
2022-05-21 19:21:24 +0700

    update list tranksaksi

--- frontend/src/components/Transaksi/ListTransaksi.js
+++ frontend/src/components/Transaksi/ListTransaksi.js
@@ -1,17 +1,14 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import jwt_decode from "jwt-decode";
-import { useNavigate } from "react-router-dom";
-import swal from "sweetalert";
-import DataTable from "react-data-table-component";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
+import swal from 'sweetalert';
+import DataTable from 'react-data-table-component';
 
 const ListTransaksi = () => {
-  // const [name, setName] = useState('');
   const [transaksis, setTransaksis] = useState('');
-  const [role, setRole] = useState("");
-  const [token, setToken] = useState("");
-  const [expired, setExpired] = useState("");
-  const [transaksi, setTransaksi] = useState([]);
+  const [token, setToken] = useState('');
+  const [expired, setExpired] = useState('');
   const history = useNavigate();
 
   const axiosJwt = axios.create();
@@ -24,40 +21,36 @@ const ListTransaksi = () => {
 
   const refreshToken = async () => {
     try {
-      const response = await axios.get("http://localhost:5000/token");
+      const response = await axios.get('http://localhost:5000/token');
       setToken(response.data.accessToken);
       const decode = jwt_decode(response.data.accessToken);
-      // setName(decode.name);
-      setRole(decode.role);
       setExpired(decode.exp);
     } catch (error) {
       if (error.response) {
-        history("/");
+        history('/');
       }
     }
   };
 
   axiosJwt.interceptors.request.use(
-    async (config) => {
+    async config => {
       const currentDate = new Date();
       if (expired * 1000 < currentDate.getTime()) {
-        const response = await axios.get("http://localhost:5000/token");
+        const response = await axios.get('http://localhost:5000/token');
         config.headers.Authorization = `Bearer ${response.data.accessToken}`;
         setToken(response.data.accessToken);
         const decode = jwt_decode(response.data.accessToken);
-        // setName(decode.name);
-        setRole(decode.role);
         setExpired(decode.exp);
       }
       return config;
     },
-    (error) => {
+    error => {
       return Promise.reject(error);
     }
   );
 
   const getTransaksis = async () => {
-    const response = await axiosJwt.get("http://localhost:5000/transaksis", {
+    const response = await axiosJwt.get('http://localhost:5000/transaksis', {
       headers: {
         Authorization: `Bearer ${token}`,
       },
@@ -65,85 +58,107 @@ const ListTransaksi = () => {
     setTransaksis(response.data);
   };
 
-  function addTransaksi(id) {
-    history("/add-transaction/_add");
+  function addTransaksi() {
+    history('/add-transaction/_add');
   }
 
   function editTransaksi(id) {
-    history(`/edit-transaction/${id}`);
+    history(`/add-transaction/${id}`);
   }
 
   function viewTransaksi(id) {
     history(`/view-transaction/${id}`);
   }
 
-  const deleteTransaksi = async (id) => {
-    var proceed = window.confirm("Apakah anda yakin hapus?");
+  const deleteTransaksi = async id => {
+    var proceed = window.confirm('Apakah anda yakin hapus?');
     if (proceed) {
-      const response = await axiosJwt.delete("http://localhost:5000/transaksi/" + id, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      const response = await axiosJwt.delete(
+        'http://localhost:5000/transaksi/' + id,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
       swal(response.data.msg);
-      const NewTransaksis = transaksis.filter((Transaksi) => Transaksi.id !== id);
+      const NewTransaksis = transaksis.filter(Transaksi => Transaksi.id !== id);
       setTransaksis(NewTransaksis);
     } else {
       // swal('batal hapus');
     }
   };
+  const numberWithComas = harga => {
+    return harga.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+  };
 
   const columns = [
     {
-      name: "Id",
-      width: "50px",
-      cell: (row) => {
-        return <div>{row.id}</div>;
+      name: 'No',
+      width: '50px',
+      id: 'row',
+      cell: (row, index) => {
+        return <div>{index + 1}</div>;
       },
       shortable: true,
     },
     {
-      name: "Tgl",
-      selector: (row) => row.tgl,
-      width: "200px",
+      name: 'Tgl',
+      selector: row => row.tgl,
+      width: '200px',
       sortable: true,
     },
     {
-      name: "Name",
-      selector: (row) => row.nama_customer,
-      width: "200px",
+      name: 'Name',
+      selector: row => row.nama_customer,
+      width: '200px',
       sortable: true,
     },
     {
-      name: "Produk",
-      selector: (row) => row.produk,
-      width: "200px",
+      name: 'Produk',
+      selector: row => row.produk,
+      width: '200px',
       sortable: true,
     },
     {
-      name: "Harga",
-      selector: (row) => row.harga,
-      width: "200px",
+      name: 'Harga',
+      selector: row => numberWithComas(row.harga),
+      width: '200px',
       sortable: true,
     },
     {
-      name: "Qty",
-      selector: (row) => row.qty,
-      width: "200px",
+      name: 'Qty',
+      selector: row => row.qty,
+      width: '200px',
       sortable: true,
     },
     {
-      name: "Action",
-      width: "400px",
-      cell: (row) => (
+      name: 'Total Penjualan',
+      selector: row => numberWithComas(row.qty * row.harga),
+      width: '200px',
+      sortable: true,
+    },
+    {
+      name: 'Action',
+      width: '400px',
+      cell: row => (
         <div>
-          <button onClick={() => editTransaksi(row.id)} className="button is-default mr-2">
+          <button
+            onClick={() => editTransaksi(row.id)}
+            className='button is-default mr-2'
+          >
             Edit
           </button>
-          <button onClick={() => deleteTransaksi(row.id)} className="button is-danger mr-2">
+          <button
+            onClick={() => deleteTransaksi(row.id)}
+            className='button is-danger mr-2'
+          >
             Delete
           </button>
-          <button onClick={() => viewTransaksi(row.id)} className="button is-success mr-2">
+          <button
+            onClick={() => viewTransaksi(row.id)}
+            className='button is-success mr-2'
+          >
             View
           </button>
         </div>
@@ -162,7 +177,7 @@ const ListTransaksi = () => {
     return (
       <div>
         <h3>Data Transaksi</h3>
-        <button onClick={addTransaksi} className="button is-info">
+        <button onClick={addTransaksi} className='button is-info'>
           Add Transaksi
         </button>
         <DataTable columns={columns} data={data} pagination />
@@ -171,7 +186,7 @@ const ListTransaksi = () => {
   }
 
   return (
-    <div className="container mt-5">
+    <div className='container mt-5'>
       <h1>Transaksi</h1>
       <hr></hr>
       {MyComponent()}
